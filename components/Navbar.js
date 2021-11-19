f1fb3d910448548b00d2b2b085b629c8324e3673
sphade <sphade010@gmail.com>
2021-11-19 05:52:35 -1200

    nav bar update

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,8 +5,8 @@ import { ChatIcon, BellIcon, UserCircleIcon } from "@heroicons/react/outline";
 import NavIcon from "./NavIcon";
 function Navbar() {
   return (
-    <header className="sticky z-10 top-0 left-0 bg-green-50 sm:flex items-center px-3 shadow  justify-between">
-      <div className=" flex items-center justify-between space-x-4 md:space-x-2">
+    <header className="sticky z-10 top-0 left-0 bg-green-50 sm:flex items-center px-2 sm:px-5 md:px-10 shadow  justify-between">
+      <div className=" flex items-center justify-between space-x-10 md:space-x-5">
         <span className="px-3 py-1 bg-green-500 inline-block font-black text-gray-100 text-2xl rounded-md">
           w
         </span>
@@ -15,26 +15,27 @@ function Navbar() {
           <input
             type="text"
             placeholder="search naijawave"
-            className=" focus-within:outline-none bg-transparent px-2 hidden md:block"
+            className=" focus-within:outline-none bg-transparent px-2 hidden lg:block"
           />
         </form>
       </div>
       <div className="flex sm:contents ">
-        <div className="  flex items-center  justify-between    flex-grow sm:justify-center  ">
+        <div className="  flex items-center  justify-between     flex-grow sm:justify-center  ">
           <NavIcon Icon={HomeIcon} link="/" active />
           <NavIcon Icon={ChatIcon} link="/" />
           <NavIcon Icon={BellIcon} link="/" />
           <NavIcon Icon={UserCircleIcon} link="/" />
+         
         </div>
 
-        <div className="flex  space-x-1  lg:space-x-5 text-sm md:space-x-2  justify-between  items-center ">
-          <h5 className="text-sm lg:text-base font-bold ">
+        <div className="flex  space-x-1  lg:space-x-20 text-sm md:space-x-16  justify-between  items-center ">
+          <h5 className="text-xs lg:text-base font-bold ">
             Wave,
-            <span className="text-sm  lg:text-base font-black  sm:inline-block">
+            <span className="  font-black  inline-block">
               ADEBOLA
             </span>
           </h5>
-          <ChevronDownIcon className="  w-6 h-6 sm:h-7 sm:w-7 md:w-8 md:h-8 lg:w-9 lg:h-9  bg-green-700 text-gray-200 rounded-full" />
+          <ChevronDownIcon className="  h-7 w-7 md:w-8 md:h-8 lg:w-9 lg:h-9  bg-green-700 text-gray-200 rounded-full" />
         </div>
       </div>
     </header>
