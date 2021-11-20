55a1cb343dbbefd55a5826cb59afeedceb079d00
sphade <sphade010@gmail.com>
2021-11-19 20:52:42 -1200

    navbar update 2

--- components/NavIcon.js
+++ components/NavIcon.js
@@ -1,19 +1,26 @@
 import React from "react";
 
 import Link from "next/link";
-function NavIcon({ Icon, link, active }) {
+function NavIcon({ Icon, link, active, name }) {
   return (
     <Link href={link}>
       <a
-        className={`transition ease-in duration-200 py-2 border-b-2 inline-block md:hover:bg-green-100   px-3 md:px-5 lg:px-7 ${
-          active ? " border-green-500 border-b-2 " : ""
+        className={`transition ease-in duration-200 py-2  flex items-center hover:bg-green-100   px-3 md:px-5 lg:px-7 
+        
         }`}
       >
         <Icon
-          className={`text-gray-700  w-6 h-6 sm:h-7 sm:w-7 md:w-8 md:h-8 ${
-            active ? " text-green-900  " : ""
+          className={`text-gray-600  w-5 h-5 sm:h-7 sm:w-7  ${
+            active ? " text-green-800  " : ""
           }  `}
         />
+        <p
+          className={`text-xs sm:text-sm md:font-bold capitalize font-semibold text-gray-600  ${
+            active ? " text-green-800  " : ""
+          }`}
+        >
+          {name}
+        </p>
       </a>
     </Link>
   );
--- components/Navbar.js
+++ components/Navbar.js
@@ -5,12 +5,12 @@ import { ChatIcon, BellIcon, UserCircleIcon } from "@heroicons/react/outline";
 import NavIcon from "./NavIcon";
 function Navbar() {
   return (
-    <header className="sticky z-10 top-0 left-0 bg-green-50 sm:flex items-center px-2 sm:px-5 md:px-10 shadow  justify-between">
+    <header className="sticky z-10 top-0 left-0 bg-green-50 sm:flex items-center px-3 md:px-3 shadow  justify-between">
       <div className=" flex items-center justify-between space-x-10 md:space-x-5">
         <span className="px-3 py-1 bg-green-500 inline-block font-black text-gray-100 text-2xl rounded-md">
           w
         </span>
-        <form className="flex items-center bg-green-100  p-2 rounded-md  ">
+        <form className="flex items-center bg-green-100  p-2 rounded-full md:rounded-lg ">
           <SearchIcon className="text-green-500 w-6 h-6 cursor-pointer" />
           <input
             type="text"
@@ -20,22 +20,19 @@ function Navbar() {
         </form>
       </div>
       <div className="flex sm:contents ">
-        <div className="  flex items-center  justify-between     flex-grow sm:justify-center  ">
-          <NavIcon Icon={HomeIcon} link="/" active />
-          <NavIcon Icon={ChatIcon} link="/" />
-          <NavIcon Icon={BellIcon} link="/" />
-          <NavIcon Icon={UserCircleIcon} link="/" />
-         
+        <div className="  flex items-center  justify-between overflow-auto    flex-grow sm:justify-center  ">
+          <NavIcon Icon={HomeIcon} link="/" active name='home'/>
+          <NavIcon Icon={ChatIcon} link="/chat" name='messaging' />
+          <NavIcon Icon={BellIcon} link="/" name='notification' />
+          <NavIcon Icon={UserCircleIcon} link="/" name='profile' />
         </div>
 
-        <div className="flex  space-x-1  lg:space-x-20 text-sm md:space-x-16  justify-between  items-center ">
-          <h5 className="text-xs lg:text-base font-bold ">
+        <div className="flex  justify-between  items-center flex-shrink">
+          <h5 className="text-xs lg:text-sm font-bold hidden md:block">
             Wave,
-            <span className="  font-black  inline-block">
-              ADEBOLA
-            </span>
+            <span className="  font-black  inline-block">ADEBOLA</span>
           </h5>
-          <ChevronDownIcon className="  h-7 w-7 md:w-8 md:h-8 lg:w-9 lg:h-9  bg-green-700 text-gray-200 rounded-full" />
+          <ChevronDownIcon className=" ml-2 md:ml-5 h-5 w-5 md:w-7 md:h-7 lg:w-8 lg:h-8  bg-green-700 text-gray-200 rounded-full" />
         </div>
       </div>
     </header>
