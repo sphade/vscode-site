221ff804983400680ee75f59dda137f531719b42
Lawal Adebola <85949974+sphade@users.noreply.github.com>
2022-01-14 19:19:42 +0100

    Layout fix 2

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -14,7 +14,7 @@ const Portfolio = () => {
  
   }, []);
   return (
-    <div className="  bg-primary layout-spacing min-h-full">
+    <div className="  bg-primary layout-spacing min-h-screen">
       <Head>
         <title>my Project</title>
         <meta name="description" content="lawal Adebola portfolio website" />
